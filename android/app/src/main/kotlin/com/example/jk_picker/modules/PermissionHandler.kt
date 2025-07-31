5621021f86cdfcaa253e7ac4f46325e59e5b5ce5
nguyentrinh2611 <nguyentrinha3qt@gmail.com>
2025-07-31 17:23:03 +0700

    feat: method get list album name

---
+++ android/app/src/main/kotlin/com/example/jk_picker/modules/PermissionHandler.kt
@@ -0,0 +1,31 @@
+package com.example.jk_picker.modules
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class PermissionHandler {
+    private val IMAGE_PERMISSION_REQUEST_CODE = 2001
+
+    fun hasPermission(activity: Activity): Boolean{
+        return if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.TIRAMISU){
+            ContextCompat.checkSelfPermission(activity, android.Manifest.permission.READ_MEDIA_IMAGES)==PackageManager.PERMISSION_GRANTED
+
+        }else {
+            ContextCompat.checkSelfPermission(activity, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        }
+
+    }
+
+    fun requestImagePermission(activity: Activity){
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.TIRAMISU){
+            ActivityCompat.requestPermissions(activity, arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES),IMAGE_PERMISSION_REQUEST_CODE )
+
+        }else {
+            ActivityCompat.requestPermissions(activity, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),IMAGE_PERMISSION_REQUEST_CODE )
+
+        }
+    }
+}
